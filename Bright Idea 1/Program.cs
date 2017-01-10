95b36b0411ed68eb5fdfb544b7e8438d01a2613d
miha2158 <miha.vanya@gmail.com>
2017-01-10 19:21:12 +0500

    NumberSystems
    A converter that doesn't work

---
+++ Bright Idea 1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bright_Idea_1
+{
+    class Program
+    {
+        static void ToMore(string Input, int i, int BaseBeg, int BaseEnd, char[] symbols, ref string Output)
+        {
+            Input.Reverse();
+            int j, k, L = Input.Length;
+
+            while (Input != string.Empty)
+            {
+                for (j = 1; symbols[j] != Input[i]; j++) ;
+
+
+                Input = symbols[j - 1] + Input.Remove(0, 1);
+                Output =S Output.Remove(0,1)
+
+
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Convert number ");
+            string Input = Console.ReadLine();
+            Console.WriteLine();
+
+
+            bool Negative = false;
+            if (Input[1] == '-')
+            {
+                Negative = true;
+                Input = Input.Remove(0, 1);
+            }
+
+            int BaseBeg, BaseEnd;
+
+            Console.Write("From base ");
+            while (!int.TryParse(Console.ReadLine(), out BaseBeg))
+                Console.WriteLine("Try again");
+            
+            Console.WriteLine();
+
+            Console.Write("To base ");
+            while (!int.TryParse(Console.ReadLine(), out BaseEnd))
+                Console.WriteLine("Try again");
+            
+            Console.WriteLine();
+
+            char[] symbols = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            string Output = string.Empty;
+
+            if (BaseEnd < BaseBeg)
+            {
+                
+            }
+            else
+            {
+             
+            }
+
+            if (Negative)
+                Output = "-" + Output;
+            Console.WriteLine("Number is {0}", Output);
+            
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ numberSys/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberSys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Convert number ");
+            string Input = Console.ReadLine();
+            Console.WriteLine();
+
+            int BaseStart, BaseTarget;
+
+            Console.Write("From base ");
+            while (!int.TryParse(Console.ReadLine(), out BaseStart))
+            {
+                Console.WriteLine("Try again");
+            }
+            Console.WriteLine();
+
+            Console.Write("To base ");
+            while (!int.TryParse(Console.ReadLine(), out BaseTarget))
+            {
+                Console.WriteLine("Try again");
+            }
+            Console.WriteLine();
+
+
+            int NInp = 0;// Convert.ToInt32(Input);
+            string result = string.Empty;
+
+            int len = Input.Length;
+            int i;
+            for (i = len; i <= 1; i--)
+            {
+                int num1;
+
+                if (!int.TryParse(Input[i].ToString(), out num1))
+                    num1 = char.ConvertToUtf32(Input[i].ToString(), 0) - char.ConvertToUtf32("A", 0) + 10;
+
+                NInp = NInp + Convert.ToInt32(Math.Pow(10, i - 1)) * num1;
+                                
+            }
+
+            /*do
+            {
+                string symbol;
+                if (NInp % BaseTarget > 9)
+                    symbol = char.ConvertFromUtf32(char.ConvertToUtf32("A", 0) + NInp % BaseTarget - 10).ToString();
+                else symbol = (NInp % BaseTarget).ToString();
+
+                result = symbol + result;
+                NInp /= BaseTarget;
+            }
+            while (NInp > 0);*/
+
+            Console.WriteLine("Result is {0}",NInp);
+
+            Console.ReadKey(true);
+        }
+    }
+}
